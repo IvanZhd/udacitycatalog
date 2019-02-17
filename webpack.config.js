db25e94b51744cc809224bbee1ec322e79dfb133
Ivan <ivan.zhdanov@hotmail.com>
2019-02-17 15:37:41 +0100

    feat: Add title and rename bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,12 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Custom template',
+      title: 'Output management',
       template: './src/index.html'
     })
   ]
