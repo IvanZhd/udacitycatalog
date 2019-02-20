f8b397ba6d9d20d61e211d302061e9034b3844f4
Ivan <ivan.zhdanov@hotmail.com>
2019-02-20 11:11:44 +0100

    feat: Add hot module replacement

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
 import _ from 'lodash';
 import printMe from './print.js';
+import './styles.css';
 
 function component() {
-    let element = document.createElement('div');
-    var btn = document.createElement('button');
-  
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  let element = document.createElement('div');
+  var btn = document.createElement('button');
 
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
-    
-    element.appendChild(btn);
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-    return element;
-  }
-  
-  document.body.appendChild(component());
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
+  return element;
+}
+
+let element = component(); // Store the element to re-render on print.js changes
+document.body.appendChild(element);
+
+if (module.hot) {
+  module.hot.accept('./print.js', function () {
+    console.log('Accepting the updated printMe modul!');
+    printMe();
+  })
+}
